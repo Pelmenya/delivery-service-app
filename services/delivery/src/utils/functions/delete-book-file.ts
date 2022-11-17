2aaf7d91d7e78cf4b34e1d601547b95ef40be23f
Dmitry Lyapin <lyapindm@yandex.ru>
2022-11-17 18:55:21 +0300

    Сделал signup

--- services/delivery/src/utils/functions/delete-book-file.ts
+++ services/delivery/src/utils/functions/delete-book-file.ts
@@ -1,9 +1,9 @@
-import { notDeleteFiles, publicBooksFilesDir } from '../constants/constants';
+import { notDeleteFiles, publicFilesDir } from '../constants/constants';
 import fs from 'fs';
 
 export const deleteBookFile = (file: string) => {
     if (!notDeleteFiles.includes(file)) {
-        fs.unlink(`${publicBooksFilesDir}/${file}`, (err) => {
+        fs.unlink(`${publicFilesDir}/${file}`, (err) => {
             if (err && err.code == 'ENOENT') {
                 console.info('File doesn`t exist, won`t remove it.');
             } else if (err) {
